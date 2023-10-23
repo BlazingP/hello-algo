c5cef4fcc16a472e3817de3e3ccd78749b9aada4
BlazingP <35942486+BlazingP@users.noreply.github.com>
2023-10-23 22:45:47 +0900

    Inspector插件

--- codes/Vue/vue-project/vite.config.ts
+++ codes/Vue/vue-project/vite.config.ts
@@ -3,12 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Inspector from 'vite-plugin-inspect'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    Inspector(),
   ],
   resolve: {
     alias: {
